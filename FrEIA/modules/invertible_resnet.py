52aadaa0ffb977f9517c24df2053ae8c31e9b81f
Barrett Ames <cbames@gmail.com>
2020-10-14 12:15:08 -0400

    remove actnorm in resnet

--- FrEIA/modules/invertible_resnet.py
+++ FrEIA/modules/invertible_resnet.py
@@ -6,57 +6,57 @@ import numpy as np
 
 
 
-class ActNorm(nn.Module):
+# class ActNorm(nn.Module):
 
-    def __init__(self, dims_in, init_data=None):
-        super().__init__()
-        self.dims_in = dims_in[0]
-        param_dims = [1, self.dims_in[0]] + [1 for i in range(len(self.dims_in) - 1)]
-        self.scale = nn.Parameter(torch.zeros(*param_dims))
-        self.bias = nn.Parameter(torch.zeros(*param_dims))
+#     def __init__(self, dims_in, init_data=None):
+#         super().__init__()
+#         self.dims_in = dims_in[0]
+#         param_dims = [1, self.dims_in[0]] + [1 for i in range(len(self.dims_in) - 1)]
+#         self.scale = nn.Parameter(torch.zeros(*param_dims))
+#         self.bias = nn.Parameter(torch.zeros(*param_dims))
 
-        if init_data:
-            self.initialize_with_data(init_data)
-        else:
-            self.init_on_next_batch = True
+#         if init_data:
+#             self.initialize_with_data(init_data)
+#         else:
+#             self.init_on_next_batch = True
 
-        def on_load_state_dict(*args):
-            # when this module is loading state dict, we SHOULDN'T init with data,
-            # because that will reset the trained parameters. Registering a hook
-            # that disable this initialisation.
-            self.init_on_next_batch = False
-        self._register_load_state_dict_pre_hook(on_load_state_dict)
+#         def on_load_state_dict(*args):
+#             # when this module is loading state dict, we SHOULDN'T init with data,
+#             # because that will reset the trained parameters. Registering a hook
+#             # that disable this initialisation.
+#             self.init_on_next_batch = False
+#         self._register_load_state_dict_pre_hook(on_load_state_dict)
 
-    def initialize_with_data(self, data):
-        # Initialize to mean 0 and std 1 with sample batch
-        # 'data' expected to be of shape (batch, channels[, ...])
-        assert all([data.shape[i+1] == self.dims_in[i] for i in range(len(self.dims_in))]),\
-            "Can't initialize ActNorm layer, provided data don't match input dimensions."
-        self.scale.data.view(-1)[:] \
-            = torch.log(1 / data.transpose(0,1).contiguous().view(self.dims_in[0], -1).std(dim=-1))
-        data = data * self.scale.exp()
-        self.bias.data.view(-1)[:] \
-            = -data.transpose(0,1).contiguous().view(self.dims_in[0], -1).mean(dim=-1)
-        self.init_on_next_batch = False
+#     def initialize_with_data(self, data):
+#         # Initialize to mean 0 and std 1 with sample batch
+#         # 'data' expected to be of shape (batch, channels[, ...])
+#         assert all([data.shape[i+1] == self.dims_in[i] for i in range(len(self.dims_in))]),\
+#             "Can't initialize ActNorm layer, provided data don't match input dimensions."
+#         self.scale.data.view(-1)[:] \
+#             = torch.log(1 / data.transpose(0,1).contiguous().view(self.dims_in[0], -1).std(dim=-1))
+#         data = data * self.scale.exp()
+#         self.bias.data.view(-1)[:] \
+#             = -data.transpose(0,1).contiguous().view(self.dims_in[0], -1).mean(dim=-1)
+#         self.init_on_next_batch = False
 
-    def forward(self, x, rev=False):
-        if self.init_on_next_batch:
-            self.initialize_with_data(x[0])
+#     def forward(self, x, rev=False):
+#         if self.init_on_next_batch:
+#             self.initialize_with_data(x[0])
 
-        if not rev:
-            return [x[0] * self.scale.exp() + self.bias]
-        else:
-            return [(x[0] - self.bias) / self.scale.exp()]
+#         if not rev:
+#             return [x[0] * self.scale.exp() + self.bias]
+#         else:
+#             return [(x[0] - self.bias) / self.scale.exp()]
 
-    def jacobian(self, x, rev=False):
-        if not rev:
-            return (self.scale.sum() * np.prod(self.dims_in[1:])).repeat(x[0].shape[0])
-        else:
-            return -self.jacobian(x)
+#     def jacobian(self, x, rev=False):
+#         if not rev:
+#             return (self.scale.sum() * np.prod(self.dims_in[1:])).repeat(x[0].shape[0])
+#         else:
+#             return -self.jacobian(x)
 
-    def output_dims(self, input_dims):
-        assert len(input_dims) == 1, "Can only use 1 input"
-        return input_dims
+#     def output_dims(self, input_dims):
+#         assert len(input_dims) == 1, "Can only use 1 input"
+#         return input_dims
 
 
 
